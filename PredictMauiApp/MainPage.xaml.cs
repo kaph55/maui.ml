2cbc3d3f4a2decf3671e78a6e132b7682faa7156
Practice-1 <kaph5@studenr.eadania.dk>
2026-04-19 21:00:37 +0200

    initial

---
+++ PredictMauiApp/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace PredictMauiApp
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly MLModel _model;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            _model = new MLModel();
+        }
+
+        private void OnPredictClicked(object sender, EventArgs e)
+        {
+            if (float.TryParse(InputBox.Text, out float x))
+            {
+                float prediction = _model.Predict(x);
+
+                ResultLabel.Text = $"Prediction: {prediction}";
+            }
+            else
+            {
+                ResultLabel.Text = "Please enter a valid number";
+            }
+        }
+
+
+    }
+}
